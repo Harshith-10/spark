e48ca950d9780e9f71657f2aecd5481e73161046
Maximus5470 <gauthamsharma2005@gmail.com>
2025-05-02 10:20:29 +0530

    Functioning Google auth is set up

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -9,8 +9,6 @@ import {Checkbox} from "@/components/ui/checkbox";
 import {Input} from "@/components/ui/input";
 import {Label} from "@/components/ui/label";
 import {AlertCircle} from 'lucide-react';
-import {Separator} from '@/components/ui/separator';
-import {GoogleSignInButton} from "@/components/google-signin-button";
 
 const LoginForm = ()=> {
   const router = useRouter();
